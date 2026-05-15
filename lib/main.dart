ac783452b8ef6380bbb00974803f931f2c14cee1
Chamuditha Lahiru <chlahiru8656@gmail.com>
2026-05-15 11:21:48 +0530

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:freely/firebase_options.dart';
 import 'screens/login_screen.dart';
 import 'theme/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const FreelyApp());
 }
 
